cca4c4db7dace752e0c421e236fa85b3630fb379
ooghenekaro <114274865+ooghenekaro@users.noreply.github.com>
2024-04-14 22:25:33 +0200

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,7 @@
 def call(){
-      steps {
+      
              echo 'Checking out our code'
             git branch: 'main', url: 'https://github.com/ooghenekaro/Petclinic-tomcat-deploy.git'
-      }
+      
 
 }
