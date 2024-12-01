5966130b503064b575875b0ad093be0edc1dbb7d
onlydurodola <nowonlywork@gmail.com>
2024-12-01 03:36:34 +0100

    corrected syntax error

--- vars/deployDev.groovy
+++ vars/deployDev.groovy
@@ -1,4 +1,4 @@
-def call (){
+def call(){
       echo "Deploying our war file into our tomcat dev server"
       sshagent(['ubuntu']) {
       sh "scp -o StrictHostKeyChecking=no target/petclinic.war tomcat@3.83.93.111:/opt/tomcat/webapps"
