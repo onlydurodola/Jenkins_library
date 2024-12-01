2315c27f610ff167c110bbf97c34241f1dcd3ceb
onlydurodola <nowonlywork@gmail.com>
2024-12-01 03:32:21 +0100

    Update deployStage.groovy

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -2,7 +2,7 @@ def call(){
   echo "Deploying our war file into our tomcat Stage server"
 
                 timeout(time: 8, unit: "MINUTES") {
-input message: 'Can i deploy to stage ?', parameters: [choice(choices: ['Yes', 'No'], name: 'Stage-approval')], submitter: 'Karo-admin', submitterParameter: 'admin'        }
+input message: 'Can i deploy to stage ?', parameters: [choice(choices: ['Yes', 'No'], name: 'Stage-approval')], submitter: 'Kovu-admin', submitterParameter: 'admin'        }
                 sshagent(['ubuntu']) {
                      sh "scp -o StrictHostKeyChecking=no target/petclinic.war tomcat@18.130.34.244:/opt/tomcat/webapps"
 
