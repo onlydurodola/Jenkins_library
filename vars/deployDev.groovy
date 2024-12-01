98b0a84aed1abdbdfd2990fa12176e4ef21f0a2a
onlydurodola <nowonlywork@gmail.com>
2024-12-01 03:22:57 +0100

    corrected the syntax

--- vars/deployDev.groovy
+++ vars/deployDev.groovy
@@ -2,4 +2,4 @@ def call (){
       echo "Deploying our war file into our tomcat dev server"
       sshagent(['ubuntu']) {
       sh "scp -o StrictHostKeyChecking=no target/petclinic.war tomcat@18.130.34.244:/opt/tomcat/webapps"
-      }
+}
