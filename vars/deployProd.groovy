789ded76c52dee49960e54ad1717dcd1f80b9e9b
onlydurodola <nowonlywork@gmail.com>
2024-12-01 03:32:03 +0100

    Update deployProd.groovy

--- vars/deployProd.groovy
+++ vars/deployProd.groovy
@@ -2,7 +2,7 @@ def call(){
   echo "Deploying our war file into our tomcat prod server"
 
                 timeout(time: 8, unit: "MINUTES") {
-input message: 'Can i deploy to prod ?', parameters: [choice(choices: ['Yes', 'No'], name: 'Prod-approval')], submitter: 'Anil-admin', submitterParameter: 'admin'        }
+input message: 'Can i deploy to prod ?', parameters: [choice(choices: ['Yes', 'No'], name: 'Prod-approval')], submitter: 'Kovu-admin', submitterParameter: 'admin'        }
                 sshagent(['ubuntu']) {
                      sh "scp -o StrictHostKeyChecking=no target/petclinic.war tomcat@18.130.34.244:/opt/tomcat/webapps"
 }
