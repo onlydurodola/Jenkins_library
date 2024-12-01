20b5a46df0cf1c248c9bbf541e6cdec9fe437f2e
onlydurodola <nowonlywork@gmail.com>
2024-12-01 03:35:55 +0100

    updated development server

--- vars/deployDev.groovy
+++ vars/deployDev.groovy
@@ -1,5 +1,5 @@
 def call (){
       echo "Deploying our war file into our tomcat dev server"
       sshagent(['ubuntu']) {
-      sh "scp -o StrictHostKeyChecking=no target/petclinic.war tomcat@18.130.34.244:/opt/tomcat/webapps"
+      sh "scp -o StrictHostKeyChecking=no target/petclinic.war tomcat@3.83.93.111:/opt/tomcat/webapps"
 }
